4487d5ad4835e16286ea68a95d41d8455a0a7f3b
gradTV <64608705+gradTV@users.noreply.github.com>
2023-12-20 14:58:04 +0200

    Update Unit.js

--- Unit.js
+++ Unit.js
@@ -1,14 +1,31 @@
-function testFactorial() {
-console.log(factorial(0) === 1);
-    console.log(factorial(5) === 120);
-    console.log(factorial(10) === 362880);
-    console.log(factorial(15) === 1307674368000);
+const chai = require('chai');
+const assert = chai.assert;
+const factorial = require('./factorial');
 
-    try {
-        factorial(-5);
-       console.log(false); // This line should not be reached
-     } catch (error) {
-        console.log(error instanceof Error);
-   }
-}
-testFactorial();
+describe('Функція обчислення факторіалу', function () {
+  it('Факторіал 0 має бути 1', function () {
+    assert.equal(factorial(0), 1);
+  });
+
+  it('Факторіал 1 має бути 1', function () {
+    assert.equal(factorial(1), 1);
+  });
+
+  it('Факторіал 5 має бути 120', function () {
+    assert.equal(factorial(5), 120);
+  });
+
+  it('Факторіал 10 має бути 3628800', function () {
+    assert.equal(factorial(10), 3628800);
+  });
+
+  // Додайте інші тести за потребою
+
+  it('Факторіал негативного числа має бути undefined', function () {
+    assert.isUndefined(factorial(-5));
+  });
+
+  it('Факторіал нецілого числа має бути undefined', function () {
+    assert.isUndefined(factorial(5.5));
+  });
+});
