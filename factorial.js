cee599d9b7e51234c386237f016d1ce8f646f4dc
gradTV <64608705+gradTV@users.noreply.github.com>
2023-12-20 14:54:38 +0200

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -1,22 +1,11 @@
 function factorial(n) {
-  if (n === 0) {
-   return 1;
+  if (n === 0 || n === 1) {
+    return 1;
   } else {
     return n * factorial(n - 1);
+  }
+}
 
-
-function testFactorial() {
-  console.log(factorial(0) === 1);
-   console.log(factorial(5) === 120);
-  console.log(factorial(10) === 362880);
-  console.log(factorial(15) === 1307674368000);
-
-
- try {
-   factorial(-5);
-   console.log(false); // This line should not be reached
-      } catch (error) {
-   console.log(error instanceof Error);
-      }
-               }
-testFactorial();
+// Пример использования
+const result = factorial(5);
+console.log(`Факториал числа 5: ${result}`);
